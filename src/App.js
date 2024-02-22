1e3f5c7f06548351b383762fbebdf373989e37a9
bricorne <bricepoitevineau@gmail.com>
2024-02-22 07:20:14 +0100

    corection

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/header'
+import Main from './components/main';
+import Footer from './components/footer';
+import CustomCursor from './components/cursor';
+import './styles/fonts.css';
+import './styles/style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CustomCursor />
+      <Header />
+      <hr />
+      <Main />
+      <Footer />
     </div>
   );
 }
